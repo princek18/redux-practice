0f88afede3d9dbd4931e68c028c11f790244f2cc
princek18 <princekumar181999@gmail.com>
2021-09-07 18:13:46 +0530

    'small_modification

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Input from "./components/input/Input";
 import Display from "./components/display/Display";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import addReducer from "./redux/reducer";
 
-const store = createStore(
-  addReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 function App() {
   return (
-    <Provider store={store}>
       <div className="App">
         <Header />
         <Input />
         <Display/>
         <Footer />
       </div>
-    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import addReducer from "./redux/reducer";
+
+const store = createStore(
+  addReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
